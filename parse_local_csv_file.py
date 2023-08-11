b5a0a83bb9d9d93979ee4841577d9c62d17c6001
sg406651 <sg40661@students.mimuw.edu.pl>
2023-08-11 17:10:17 +0200

    Added script 'parse_local_csv_file' that extracts csv file from zip and loads it.

---
+++ parse_local_csv_file.py
@@ -0,0 +1,30 @@
+import zipfile
+import pandas as pd
+import os
+
+
+def input_csv_from_zip(month: str):
+    """
+    :param month: format yyyymm
+    """
+    if int(month) < 201700:
+        zip_file_path = 'data/' + month[:4] + '/' + month + '-citibike-tripdata.zip'
+        csv_file_name = month + '-citibike-tripdata.csv'
+    else:
+        zip_file_path = 'data/' + month[:4] + '/' + month + '-citibike-tripdata.csv.zip'
+        csv_file_name = month + '-citibike-tripdata.csv'
+
+    with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+        # Extract the CSV file from the ZIP archive
+        zip_ref.extract(csv_file_name, 'temp_extracted_folder')
+
+    # Read the CSV file using pandas
+    csv_path = 'temp_extracted_folder/' + csv_file_name
+    data = pd.read_csv(csv_path)
+
+    # Clean up - remove the temporary extracted folder
+    os.remove(csv_path)
+    os.rmdir('temp_extracted_folder')
+
+
+input_csv_from_zip('201307')
